c3d866e742c7836027ccebc485b4b0a67f0b17c3
juliusbenjamins <julius.benjamins1@gmail.com>
2023-05-21 15:54:44 +0200

    Added palette

--- src/App.js
+++ src/App.js
@@ -80,63 +80,91 @@ function App() {
       <div className="App flex flex-col justify-between h-screen overflow-auto">
 
         {/* Header */}
-        <header className="App-header pl-2">
-            <a className="text-white drop-shadow text-2xl font-semibold" 
+        <header className="App-header p-4">
+          <div className=''>
+          <a className="text-2xl font-semibold" 
             href='#'>
-              <h1 className="text-white drop-shadow text-2xl font-semibold" 
+              <h1 className="text-4xl font-semibold" 
               onClick={refreshPage}>Tinnitus Platform</h1>
             </a>
-          <p className="text-sm">Innovatieproject Radboud Universiteit</p>
+          <p className="text-lg">Innovatieproject Radboud Universiteit</p>
+          </div>
+          <div className='Over-ons text-xl'>
+            <mark className="Over-ons px-2 font-semibold">Over ons</mark>
+          </div>
         </header>
 
         {/* Body */}
         <div className='flex items-center justify-center m-4'>
-          <div className="border-1 rounded-md shadow-md py-6 px-3 bg-white">
-            {/* Question */}
-            {!startQuestionnaire 
-            ? (<div className='text-justify p-10 max-w-xl'>
-              <h1 className='text-center text-black text-l font-semibold'>De Uitdaging</h1>
-              <p className='mb-10 text-sm'>
-              In onze omgeving valt ons op dat veel mensen tinnitus hebben, maar niet weten dat er ook behandelingsmogelijkheden zijn. Ze balen en hebben er last van, maar hebben ook het idee dat ze er maar mee moeten leven. De uitdaging waarmee wij aan de slag gaan is daarom hoe we personen bewuster kunnen maken van de mogelijkheden tot de behandeling van tinnitus.  
-              </p>
+          <div className="App-questions border-1 rounded-lg py-6 px-3">
+            <div className='Inner-questions mx-5 my-3 p-1'>
+              {/* Question */}
+              {!startQuestionnaire 
+              ? (<div className='text-justify p-5 max-w-xl'>
+                {/* First part intro */}
+                <h1 className='text-left text-black text-xl font-semibold mb-2'>
+                  <mark className='Mark-title px-1'>De Uitdaging</mark>
+                </h1>
+                <p className='mb-10 text-md'>
+                In onze omgeving valt ons op dat veel mensen tinnitus hebben, 
+                maar niet weten dat er ook behandelingsmogelijkheden zijn. 
+                Ze balen en hebben er last van, maar hebben ook het idee dat 
+                ze er maar mee moeten leven. De uitdaging waarmee wij aan de 
+                slag gaan is daarom hoe we personen bewuster kunnen maken van 
+                de mogelijkheden tot de behandeling van tinnitus.  
+                </p>
 
-              <h2 className='text-center text-black text-l font-semibold'>Tinnitus in het kort</h2> 
-              <p className='mb-10 text-sm'>
-              Patiënten met tinnitus horen een geluid dat gecreëerd wordt van binnenuit. Er zijn verschillende soorten geluiden, de meest voorkomende zijn: gebrom, ruis en een piep. Daarnaast verschilt per persoon en per moment de ernst van deze klachten. 
-              Tinnitus wordt veroorzaakt door degeneratie van haarcellen die zich in het slakkenhuis bevinden. Het gevolg hiervan is dat de signaaltransductie van en naar de hersenen verstoord raakt. Het is hierdoor mogelijk dat er verkeerde signalen worden doorgegeven. De hersenen proberen dus de functie van de haarcellen te compenseren, wat zich kan uiten in oorsuizen.
-              </p>
-              <div className='text-center'>
-                <Button variant='outlined'
-                        onClick={() => setStartQuestionnaire(true)}>Start Vragenlijst</Button>
-              </div>
-            </div>)
-            : (<div className='max-w-xl'>
-                <Question 
-                  questionText={currentQuestion.vraag} 
-                  answerType={currentQuestion.antwoordType}
-                  setAnswerFunc={setCurrentAnswer}
-                  clickHandler={handleNextClick}
-                  yesFlag={yesFlag}
-                  noFlag={noFlag}
-                  setYesFlag={setYesFlag}
-                  setNoFlag={setNoFlag}/>
-              <div className="mt-5">
-                  {(currentQuestion.antwoordType != null) && 
-                  (<div className=''>
-                    <Button
-                      variant="outlined"
-                      size="small"
-                      onClick={(e) => handleNextClick(e)}>
-                      Volgende
-                  </Button>
-                  </div>)}
-              </div>
-            </div>)}
+                {/* Second part intro */}
+                <h2 className='text-right text-black text-xl font-semibold mb-2'>
+                  <mark className='Mark-title px-1'>Tinnitus in het kort</mark>
+                </h2> 
+                <p className='mb-10 text-md'>
+                Patiënten met tinnitus horen een geluid dat gecreëerd wordt van
+                 binnenuit. Er zijn verschillende soorten geluiden, de meest 
+                 voorkomende zijn: gebrom, ruis en een piep. Daarnaast verschilt 
+                 per persoon en per moment de ernst van deze klachten. 
+                Tinnitus wordt veroorzaakt door degeneratie van haarcellen die 
+                zich in het slakkenhuis bevinden. Het gevolg hiervan is dat de 
+                signaaltransductie van en naar de hersenen verstoord raakt. Het 
+                is hierdoor mogelijk dat er verkeerde signalen worden doorgegeven. 
+                De hersenen proberen dus de functie van de haarcellen te compenseren, 
+                wat zich kan uiten in oorsuizen.
+                </p>
+
+                {/* Start questionnaire button */}
+                <div className='text-center'>
+                  <Button variant='outlined'
+                          onClick={() => setStartQuestionnaire(true)}>Start Vragenlijst</Button>
+                </div>
+              </div>)
+              : (<div className='max-w-xl'>
+                  <Question 
+                    questionText={currentQuestion.vraag} 
+                    answerType={currentQuestion.antwoordType}
+                    setAnswerFunc={setCurrentAnswer}
+                    clickHandler={handleNextClick}
+                    yesFlag={yesFlag}
+                    noFlag={noFlag}
+                    setYesFlag={setYesFlag}
+                    setNoFlag={setNoFlag}/>
+                <div className="mt-5">
+                    {(currentQuestion.antwoordType != null) && 
+                    (<div className=''>
+                      <Button
+                        variant="outlined"
+                        size="small"
+                        onClick={(e) => handleNextClick(e)}>
+                        Volgende
+                    </Button>
+                    </div>)}
+                </div>
+              </div>)}
+            </div>
           </div>
         </div>
 
         {/* Footer */}
-        <footer className='text-xs mx-5'>
+        <footer className='App-footer text-xs px-5 py-3'>
           Innovatieproject Radboud Universiteit 2023 | Door Wojtek, Dante, Savine en Julius
         </footer>
       </div>
